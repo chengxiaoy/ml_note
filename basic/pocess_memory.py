cb9c2496497f4229f5ba9f03593a2304f4766d33
e <chengyingjuly@163.com>
2019-09-23 23:13:35 +0800

    process demo

---
+++ basic/pocess_memory.py
@@ -0,0 +1,93 @@
+from multiprocessing import Pool, Process
+import numpy as np
+import time
+import os
+
+print('Global_print', os.getpid())
+
+
+def compute2( matrix):
+    m = np.random.randn(1024, 128)
+    # print(size_m_1.shape)
+    time.sleep(10.0)
+    return matrix.dot(m)
+
+class Demo:
+    def __init__(self):
+        self.hundred_m = np.random.randn(100, 128, 1024)
+
+    def run_proc(self, size):
+        print("run child processid {}:size {}".format(os.getpid(), size))
+
+    def allocate_memory(self, size):
+        size_m_1 = np.random.rand(size - 1, 128, 1024)
+        self.run_proc(size)
+        return self.compute(size_m_1)
+
+    def compute(self, matrix):
+        m = np.random.randn(1024, 128)
+        # print(size_m_1.shape)
+        time.sleep(10.0)
+        return matrix.dot(m)
+
+    def test(self, size_list):
+        p = Pool(5)
+        size_list = [10, 20, 30, 40, 50]
+
+        memory_list = []
+        since = time.time()
+        extern_memory = []
+        for size in size_list:
+            extern_memory.append(np.random.randn(size, 128, 1024))
+        # future_list = []
+
+        # for i, size in enumerate(size_list):
+        #     # res_future = p.apply_async(allocate_memory, args=(size,))
+        #     res_future = p.apply_async(compute, args=(extern_memory[i],))
+        #     future_list.append(res_future)
+
+        future_list = p.map_async(compute2, extern_memory)
+
+        supply_time = time.time()
+        print("supply time :{}".format(supply_time - since))
+        p.close()
+        p.join()
+        future_list = future_list.get()
+        for future in future_list:
+            print(future.shape)
+        print("total cost time:{}".format(time.time() - since))
+
+
+if __name__ == '__main__':
+    demo = Demo()
+    demo.test([10, 20, 30, 40, 50])
+    # p = Process(target=run_proc, args=("subprocess",))
+    # print(os.getpid())
+    # p.start()
+
+    # p = Pool(5)
+    # size_list = [10, 20, 30, 40, 50]
+    #
+    # memory_list = []
+    #
+    # since = time.time()
+    # extern_memory = []
+    # for size in size_list:
+    #     extern_memory.append(np.random.randn(size, 128, 1024))
+    # # future_list = []
+    #
+    # # for i, size in enumerate(size_list):
+    # #     # res_future = p.apply_async(allocate_memory, args=(size,))
+    # #     res_future = p.apply_async(compute, args=(extern_memory[i],))
+    # #     future_list.append(res_future)
+    #
+    # future_list = p.map_async(compute, extern_memory)
+    #
+    # supply_time = time.time()
+    # print("supply time :{}".format(supply_time - since))
+    # p.close()
+    # p.join()
+    # future_list = future_list.get()
+    # for future in future_list:
+    #     print(future.shape)
+    # print("total cost time:{}".format(time.time() - since))
